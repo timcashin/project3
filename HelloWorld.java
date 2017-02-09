083844babc55a77b0282e37b8ee338094ea1b322
Tim Cashin <timcashin@mitre.org>
2017-02-09 15:06:14 -0500

    Fixed a bug with git

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Hello, World");
         System.out.println("How are you?");
         System.out.println("Let's talk some more");
+        System.out.println("and I don't know how git is better than SVN???");
     }
 
 }
